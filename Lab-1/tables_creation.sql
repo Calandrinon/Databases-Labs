788af5b275d298b9f91b92d7cc138446b95e42d6
Calandrinon <georgesut@yahoo.com>
2020-10-10 19:37:52 +0300

    Add foreign key "ON DELETE CASCADE" and password length constraints

--- Lab-1/tables_creation.sql
+++ Lab-1/tables_creation.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS Albums_Genres, Artists_Musicians, Artists_Albums, Albums_So
 CREATE TABLE Artist 
     (ArtistId INT IDENTITY(1, 1) PRIMARY KEY,
     Name VARCHAR(50) NOT NULL,
-    EstablishmentYear SMALLINT NOT NULL)
+    EstablishmentYear SMALLINT)
 
 -- An artist can be either an individual, or a band with multiple members.
 -- Therefore, we can create a separate table for storing musicians, and an intermediary table
@@ -51,14 +51,14 @@ CREATE TABLE Song
 
 
 CREATE TABLE Albums_Songs
-    (AlbumId INT REFERENCES Album(AlbumId),
-     SongId BIGINT REFERENCES Song(SongId),
+    (AlbumId INT REFERENCES Album(AlbumId) ON DELETE CASCADE,
+     SongId BIGINT REFERENCES Song(SongId) ON DELETE CASCADE,
      PRIMARY KEY(AlbumId, SongId))
 
 
 CREATE TABLE Record
     (RecordId INT IDENTITY(1, 1) PRIMARY KEY,
-     AlbumId INT REFERENCES Album(AlbumId),
+     AlbumId INT REFERENCES Album(AlbumId) ON DELETE CASCADE,
      Price REAL NOT NULL,
      InStock INT NOT NULL,
      RecordType VARCHAR(10) NOT NULL)
@@ -67,18 +67,18 @@ CREATE TABLE Record
 CREATE TABLE ClientUser
     (UserId INT IDENTITY(1, 1) PRIMARY KEY,
      Username VARCHAR(50) NOT NULL UNIQUE,
-     Password VARCHAR(50) NOT NULL) 
+     Password VARCHAR(50) NOT NULL CHECK (LEN(Password) > 10)) 
 
 
 CREATE TABLE UserTransaction
     (TransactionId INT IDENTITY(1, 1) PRIMARY KEY,
-     UserId INT REFERENCES ClientUser(UserId),
-     RecordId INT REFERENCES Record(RecordId),
+     UserId INT REFERENCES ClientUser(UserId) ON DELETE CASCADE,
+     RecordId INT REFERENCES Record(RecordId) ON DELETE CASCADE,
      TransactionDateTime DATETIME NOT NULL)
 
 
 CREATE TABLE Review
     (ReviewId INT IDENTITY(1, 1) PRIMARY KEY,
-     UserId INT REFERENCES ClientUser(UserId),
+     UserId INT REFERENCES ClientUser(UserId) ON DELETE SET NULL,
      ReviewText TEXT NOT NULL,
      ReviewTime DATETIME NOT NULL)
