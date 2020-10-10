8384b853ab41f27252cd3728f08239ce72f02f92
Calandrinon <georgesut@yahoo.com>
2020-10-10 19:13:16 +0300

    Add automatic incrementation of primary keys and "NOT NULL" constraints

--- Lab-1/tables_creation.sql
+++ Lab-1/tables_creation.sql
@@ -1,18 +1,18 @@
 DROP TABLE IF EXISTS Albums_Genres, Artists_Musicians, Artists_Albums, Albums_Songs, Genre, Review, Song, UserTransaction, Record, Album, Artist, Musician, ClientUser;
 
 CREATE TABLE Artist 
-    (ArtistId INT PRIMARY KEY,
-    Name VARCHAR(50),
-    EstablishmentYear SMALLINT)
+    (ArtistId INT IDENTITY(1, 1) PRIMARY KEY,
+    Name VARCHAR(50) NOT NULL,
+    EstablishmentYear SMALLINT NOT NULL)
 
 -- An artist can be either an individual, or a band with multiple members.
 -- Therefore, we can create a separate table for storing musicians, and an intermediary table
 -- for storing the relationship of musicians associated with each so-called artist/band.
 
 CREATE TABLE Musician
-    (MusicianId INT PRIMARY KEY,
-     Name VARCHAR(50),
-     Date_of_birth DATE)
+    (MusicianId INT IDENTITY(1, 1) PRIMARY KEY,
+     Name VARCHAR(50) NOT NULL,
+     Date_of_birth DATE NOT NULL)
 
 
 CREATE TABLE Artists_Musicians
@@ -22,15 +22,15 @@ CREATE TABLE Artists_Musicians
 
 
 CREATE TABLE Album 
-    (AlbumId INT PRIMARY KEY,
-     Name VARCHAR(50),
-     ReleaseDate DATE,
+    (AlbumId INT IDENTITY(1, 1) PRIMARY KEY,
+     Name VARCHAR(50) NOT NULL,
+     ReleaseDate DATE NOT NULL,
      AlbumArtLink VARCHAR(200))
 
 
 CREATE TABLE Genre
-    (GenreId INT PRIMARY KEY,
-     Title VARCHAR(30))
+    (GenreId INT IDENTITY(1, 1) PRIMARY KEY,
+     Title VARCHAR(30) NOT NULL)
 
 
 CREATE TABLE Albums_Genres
@@ -46,7 +46,7 @@ CREATE TABLE Artists_Albums
 
 
 CREATE TABLE Song
-    (SongId BIGINT PRIMARY KEY,
+    (SongId BIGINT IDENTITY(1, 1) PRIMARY KEY,
      Title VARCHAR(50))
 
 
@@ -57,27 +57,28 @@ CREATE TABLE Albums_Songs
 
 
 CREATE TABLE Record
-    (RecordId INT PRIMARY KEY,
+    (RecordId INT IDENTITY(1, 1) PRIMARY KEY,
      AlbumId INT REFERENCES Album(AlbumId),
-     Price REAL,
-     InStock INT,
-     RecordType VARCHAR(10))
+     Price REAL NOT NULL,
+     InStock INT NOT NULL,
+     RecordType VARCHAR(10) NOT NULL)
 
 
 CREATE TABLE ClientUser
-    (UserId INT PRIMARY KEY,
-     Username VARCHAR(50)) 
+    (UserId INT IDENTITY(1, 1) PRIMARY KEY,
+     Username VARCHAR(50) NOT NULL UNIQUE,
+     Password VARCHAR(50) NOT NULL) 
 
 
 CREATE TABLE UserTransaction
-    (TransactionId INT PRIMARY KEY,
+    (TransactionId INT IDENTITY(1, 1) PRIMARY KEY,
      UserId INT REFERENCES ClientUser(UserId),
      RecordId INT REFERENCES Record(RecordId),
-     TransactionDateTime DATETIME)
+     TransactionDateTime DATETIME NOT NULL)
 
 
 CREATE TABLE Review
-    (ReviewId INT PRIMARY KEY,
+    (ReviewId INT IDENTITY(1, 1) PRIMARY KEY,
      UserId INT REFERENCES ClientUser(UserId),
-     ReviewText TEXT,
-     ReviewTime DATETIME)
+     ReviewText TEXT NOT NULL,
+     ReviewTime DATETIME NOT NULL)
