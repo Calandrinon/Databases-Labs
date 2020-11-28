7c9421026e287d51608e2462f8dd2b7f77938e02
Calandrinon <georgesut@yahoo.com>
2020-11-28 08:40:38 +0200

    Add one test

---
+++ Lab-4/tests.sql
@@ -0,0 +1,65 @@
+SELECT *
+FROM sys.tables
+
+CREATE OR ALTER PROCEDURE GenerateRandomString (@Output VARCHAR(50) OUTPUT)
+AS
+    DECLARE @Length INT = RAND() * 5 + 8;
+    DECLARE @CharPool VARCHAR(72) =
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ123456789.,-_!$@#%^&*'
+    DECLARE @PoolLength INT = Len(@CharPool)
+    DECLARE @LoopCount INT = 0
+    DECLARE @RandomString VARCHAR(50) = ''
+
+    WHILE (@LoopCount < @Length) BEGIN
+        SELECT @RandomString = @RandomString +
+            SUBSTRING(@Charpool, CONVERT(int, RAND() * @PoolLength), 1)
+        SELECT @LoopCount = @LoopCount + 1
+    END
+
+    SELECT @Output = @RandomString
+GO
+
+
+DECLARE @SomeDate DATETIME;
+EXEC GenerateRandomDate @SomeDate
+SELECT @SomeDate
+
+DECLARE @Result VARCHAR(50)
+EXEC GenerateRandomString @Result OUTPUT
+PRINT @Result
+
+
+CREATE OR ALTER PROCEDURE TestAlbumTableInsertionTime (@INSERTIONS INT)
+AS DECLARE @startTime DATETIME;
+    DECLARE @endTime DATETIME;
+
+    BEGIN TRY DELETE FROM Albums_Genres END TRY BEGIN CATCH END CATCH
+    BEGIN TRY DELETE FROM Review END TRY BEGIN CATCH END CATCH
+    BEGIN TRY DELETE FROM Concerts_Artists END TRY BEGIN CATCH END CATCH
+    BEGIN TRY DELETE FROM Artists_Albums END TRY BEGIN CATCH END CATCH
+    BEGIN TRY DELETE FROM Album END TRY BEGIN CATCH END CATCH
+
+    DECLARE @i INT = 0;
+    DECLARE @randomName VARCHAR(50);
+    DECLARE @randomDate VARCHAR(50);
+    DECLARE @randomLink VARCHAR(50);
+
+    SET @startTime= GETDATE();
+    WHILE @i < @INSERTIONS
+        BEGIN
+            EXEC GenerateRandomString @randomName OUTPUT
+            EXEC GenerateRandomString @randomLink OUTPUT
+            SET @randomDate = DATEADD(DAY, ABS(CHECKSUM(NEWID()) % (365 * 10) ), '2011-01-01')
+            PRINT 'Record: ' + @randomName + ' ' + @randomDate + ' ' + @randomLink
+            INSERT INTO Album (Name, ReleaseDate, AlbumArtLink) VALUES (@randomName, @randomDate, @randomLink)
+            SET @i = @i + 1;
+        END
+
+    SET @endTime= GETDATE();
+    SELECT DATEDIFF(millisecond,@startTime,@endTime) AS elapsed_ms;
+GO
+
+EXEC TestArtistTableInsertionTime @INSERTIONS = 500
+SELECT * FROM Album
+
+
