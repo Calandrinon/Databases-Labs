761227b854487968bd9cd29f4d6c07d4c4c3aaa8
Calandrinon <georgesut@yahoo.com>
2020-12-12 14:26:57 +0200

    Add more queries
    TODO: Create a non-clustered index and test if the query b1 runs faster

--- Lab-5/queries.sql
+++ Lab-5/queries.sql
@@ -24,4 +24,11 @@ CREATE TABLE Albums_Genres
 --  2)
         SELECT * FROM Album A WHERE A.AlbumId >= 100 AND A.AlbumId <= 200
 --  3)
-        SELECT * FROM Genre G
+        SELECT GenreId, Title FROM Genre G
+--  4)
+        SELECT GenreId, Title FROM Genre G WHERE Title = 'Progressive Rock'
+
+
+--b)
+--  1)
+        SELECT * FROM Album A WHERE A.ReleaseDate < '2000-12-30'
